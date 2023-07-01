d3365e1b520ddb3596e4446ed9406c986031bea4
brunnoaraujo0 <brunoaraujop9@gmail.com>
2023-06-30 21:51:32 -0300

    carrosel

--- src/app/components/in-progress/in-progress.component.ts
+++ src/app/components/in-progress/in-progress.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'inProgress',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./in-progress.component.css']
 })
 export class InProgressComponent {
-
+  @Input() tuduItem: any = [];
+  
+ login() {
+  console.log(this.tuduItem)
+ }
+ constructor(){
+  this.login();
+ }
 }
